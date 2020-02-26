fd689e3405517bfd1f5f67deee088c390daadc44
badMFboy <a.mm.grishin@gmail.com>
2020-02-26 14:52:50 +0300

    add doc string to auth blueprint

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,3 +1,9 @@
+"""
+Auth blueprint.
+Packet contain two modules.
+'forms.py' - contain class forms and it settings for login page.
+'routes.py' - contain handlers for URLs.
+"""
 from flask import Blueprint 
 
 bp = Blueprint('Auth', __name__)
